c7885028b6a806019ef9b8af76fc0f02f77a1923
Bear <william456821@gmail.com>
2017-03-20 22:23:39 +0800

    初步完成 service 分割

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Http, RequestOptions, Headers} from '@angular/http'
-import { Observable } from 'rxjs';
+import { DataService } from "./data.service";
 
 @Component({
   selector: 'app-root',
@@ -15,35 +14,14 @@ export class AppComponent implements OnInit {
   filterType = 'All';
   toggleAll = false;
 
-  private requestOptions = new RequestOptions({
-    headers : new Headers({
-      'authorization' : 'token 5e1356f3-4249-40ac-8201-91e6712485b4'
-    })
-  });
-
-  constructor(private http :Http){
+  constructor(private dataSvc : DataService){
   }
   ngOnInit(){
-    this.getTodos().subscribe(data => {
+    this.dataSvc.getTodos().subscribe(data => {
       this.todos = data;
     })
   }
-  getTodos(){
-    return this.http.get('./me/todomvc',this.requestOptions).map(res => {
-      return res.json();
-    }).catch(error => {
-      console.log(error);
-      return Observable.of<any[]>([]);
-    })
-  }
-  saveTodos (newTodos : any[]){
-    return this.http.post('./me/todomvc', newTodos, this.requestOptions).map(res => {
-      this.todos = res.json();
-    }).catch(error => {
-      console.log(error);
-      return Observable.of<any[]>([]);
-    });
-  }
+  
 
   addTodo(){
     let newTodos = [...this.todos];
@@ -51,14 +29,15 @@ export class AppComponent implements OnInit {
       text :this.todo,
       done : false
     });
-    this.saveTodos(newTodos).subscribe(data =>{
+    this.dataSvc.saveTodos(newTodos).subscribe(data =>{
+      this.todos = data;
       this.todo = '';
     })
   }
   clearCompleted(){
     let newTodos = this.todos.filter(item =>(!item.done));
-    this.saveTodos(newTodos).subscribe(data =>{
-      console.log(data);
+    this.dataSvc.saveTodos(newTodos).subscribe(data =>{
+      this.todos = data;
     })
   }
   filterChange(filterType : string){
@@ -67,15 +46,21 @@ export class AppComponent implements OnInit {
   toggleAllChanged(value : boolean){
     let newTodos = [...this.todos];
     newTodos.forEach(item => item.done = value);
-    this.saveTodos(newTodos).subscribe(data=>{});
+    this.dataSvc.saveTodos(newTodos).subscribe(data=>{
+      this.todos = data;
+    });
   }
   updateToggleAllState(){
     this.toggleAll = this.todos.filter(item => (!item.done)).length === 0;
-    this.saveTodos(this.todos).subscribe(data=>{});
+    this.dataSvc.saveTodos(this.todos).subscribe(data=>{
+      this.todos = data;
+    });
   }
   removeTodo(todo){
     let newTodos = [...this.todos];
     newTodos.splice(this.todos.indexOf(todo),1);
-    this.saveTodos(newTodos).subscribe(data=>{});
+    this.dataSvc.saveTodos(newTodos).subscribe(data=>{
+      this.todos = data;
+    });
   }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Headers} from '@angular/http'
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  private requestOptions = new RequestOptions({
+    headers : new Headers({
+      'authorization' : 'token 5e1356f3-4249-40ac-8201-91e6712485b4'
+    })
+  });
+
+  constructor(private http: Http) { }
+
+  getTodos(){
+    return this.http.get('./me/todomvc',this.requestOptions).map(res => {
+      return res.json();
+    }).catch(error => {
+      console.log(error);
+      return Observable.of<any[]>([]);
+    })
+  }
+  saveTodos (newTodos : any[]){
+    return this.http.post('./me/todomvc', newTodos, this.requestOptions).map(res => {
+      return res.json();
+    }).catch(error => {
+      console.log(error);
+      return Observable.of<any[]>([]);
+    });
+  }
 
 }
